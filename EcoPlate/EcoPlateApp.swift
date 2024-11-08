73347781eb621609317a6c26a84bbfc3dea507eb
Juan Daniel Rodríguez Oropeza <j_danny07@hotmail.com>
2024-11-08 17:12:23 +0100

    Initial Commit

---
+++ EcoPlate/EcoPlateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoPlateApp.swift
+//  EcoPlate
+//
+//  Created by Juan Daniel Rodriguez Oropeza on 08.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct EcoPlateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
