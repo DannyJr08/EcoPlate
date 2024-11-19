baf2900caa068cac13e6629029efeabfabd63050
Muhammadjon_Ibrohimov <Muhammadjon_Ibrohimov@student.itpu.uz>
2024-11-19 16:06:38 +0100

    commit

---
+++ EcoPlate/Views/RecipeDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  RecipeDetailView.swift
+//  EcoPlate
+//
+//  Created by Muhammadjon on 19/11/24.
+//
+
+import SwiftUI
+
+struct RecipeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RecipeDetailView()
+}
